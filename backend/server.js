b69c8f82d983cb699c44481817afc6aa3c703feb
atetheone <atevirran@gmail.com>
2022-07-29 19:42:50 +0100

    server default setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
-const express = require('express')
-const cors = require('cors');
-
+const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
 
-app.use(cors);
+require('./connection');
 
-app.listen(3000, () => {
-    console.log('App running on port *: 3000...')
-});
+
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.path + ' | q= ' + JSON.stringify(req.query, null, 2) + ' - ip: ' + req.ip);
+  next();
+})
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+routes(app);
+
+
+const listener = app.listen(process.env.PORT || 3001, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
